10575f962ea2c5ede8bc04de8a2418d574e83161
Shaantam Anand <sanand@tavisca.com>
2018-02-27 18:09:26 +0530

    initial commit

---
+++ src/CltsJsBuild/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CltsJsBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var argument = Console.ReadLine();
+            while (!string.IsNullOrEmpty(argument) || string.Compare("exit", argument, StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                Func<string> Runner = () =>
+                {
+                    //Run();
+                    return null;
+                };
+            }
+        }
+    }
+}
---
+++ src/Contracts/Tags.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class Tags
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Types Type { get; set; }
+        public List<string> Aliases { get; set; }
+        public Func<string, Func<string>> Functionality { get; set; }
+        public Predicate<Func<string>> RegisterTask { get; set; }
+    }
+
+    #region Enums
+    public enum Types
+    {
+        Build,
+        Path,
+        Input
+    }
+    #endregion
+}
